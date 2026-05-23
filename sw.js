e68753f26802e4fe6e7935b89a82a10dbbe2c3e8
root <root@localhost.localdomain>
2026-05-23 12:20:29 +0000

    Update GAS_URL to new deployment endpoint

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'sv-k9z4f';
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwGbJhEU7-ZT1Rhf6QATuWjQ412doyWLcDUSkZe8VuH6S2fYFV8gMA05kwaPnceZ0SsAQ/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzueftCFP8TDIi9wc6eEkUMobdz2yc7mfZtxT5cMBhIIVVHKl8jneA8SdpLFW6dJjRF_w/exec';
 const VAPID_PUBLIC_KEY = 'BO13tsTjl2y_vuX84DIzUbbWUgndqDKnvi7CF-9kkeK5ZBjeTRck4m5X8zKFLgN_-8erCil_UC4Ei1tE5fgmM-M';
 
 function _urlB64ToUint8Array(b64) {
